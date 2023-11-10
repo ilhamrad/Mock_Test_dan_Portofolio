f2bb585c35fe1ff3c96a426d8c5d9be307cb789c
ilhamrad <ilhamramahdan.m@gmail.com>
2023-11-10 11:06:05 +0700

    tes FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+function FizzBuzz(){
+    for(let x= 1; x <= 15; x++){
+        if(x % 3 === 0 && x % 5 === 0){
+            console.log('FizzBuzz')
+        }else if(x % 3 === 0){
+            console.log('Fizz')
+        }else if(x % 5 === 0){
+            console.log('Buzz')
+        }else{
+            console.log(x)
+        }
+    }
+}
+
+FizzBuzz()
